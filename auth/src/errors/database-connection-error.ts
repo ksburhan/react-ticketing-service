940cf6def57dc8b2ed78a2f0d1e8fec46dec491e
ksburhan <burhan.koeseler@hotmail.de>
2024-01-10 16:21:04 +0900

    auth: custom error abstract class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,11 +1,16 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
 
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
     reason = 'Error connecting to db..';
 
     constructor(){
-        super();
+        super('Error connecting to db');
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
     }
 
+    serializeError(){
+        return [{ message: this.reason }];
+    }
 }
