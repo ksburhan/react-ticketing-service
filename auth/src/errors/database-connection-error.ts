f957f212d7fddd27f2b579be55343410b6e29613
ksburhan <burhan.koeseler@hotmail.de>
2024-01-10 15:48:51 +0900

    auth: add error handling
    add custom errors

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,11 @@
+export class DatabaseConnectionError extends Error {
+
+    reason = 'Error connecting to db..';
+
+    constructor(){
+        super();
+
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+}
