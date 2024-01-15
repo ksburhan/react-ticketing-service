6adfe7268b4a9f91fd2a15cfecae028a6df2d37f
ksburhan <burhan.koeseler@hotmail.de>
2024-01-15 22:14:46 +0900

    auth: set request validation to middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import { User } from '../models/users';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
@@ -16,13 +17,9 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-
+    ],
+    validateRequest, 
+    async (req: Request, res: Response) => {
     const { email, password } = req.body;
     
     const existingUser = await User.findOne({ email });
