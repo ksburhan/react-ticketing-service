99cc8ac7b494983649ca8bba7eca8dbb5648c49a
ksburhan <burhan.koeseler@hotmail.de>
2024-01-16 21:08:59 +0900

    auth: add tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,6 +4,8 @@ import { app } from '../app';
 
 let mongo: any;
 beforeAll(async () => {
+    process.env.JWT_KEY = 'asdf';
+
     mongo = await MongoMemoryServer.create();
     const mongoUri = mongo.getUri();
     await mongoose.connect(mongoUri, {});
