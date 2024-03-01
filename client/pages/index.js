5796fc63c6d2e39f620f9d17b93c4a0a302c8dd8
ksburhan <burhan.koeseler@hotmail.de>
2024-03-01 18:20:20 +0900

    reusable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import buildClient from '../api/build-client';
+
 const LandingPage = ({ currentUser }) => {
     console.log(currentUser);
     // axios.get('/api/users/currentuser').catch((err) => {
@@ -9,25 +11,10 @@ const LandingPage = ({ currentUser }) => {
     return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === 'undefined') {
-        //server
-        const { data } = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-            {
-                headers: req.headers
-            }
-        ).catch((err) => {
-            console.log(err.message);
-        });
-        return data;
-    } else {
-        //browser
-        const { data } = axios.get('/api/users/currentuser').catch((err) => {
-            console.log(err.message);
-        });
-        return data;
-    }
+LandingPage.getInitialProps = async (context) => {
+    const { data } = await buildClient(context).get('/api/users/currentUser');
+
+    return data;
 }
 
 export default LandingPage;
