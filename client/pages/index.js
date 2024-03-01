e83636df3a2f605d67db506a05bf3a13ed150058
ksburhan <burhan.koeseler@hotmail.de>
2024-03-01 17:15:15 +0900

    resolve pod url getInitialProps now properly calls api without cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,35 @@
-const index = () => {
-    return <h1>Landing page </h1>
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
+    // axios.get('/api/users/currentuser').catch((err) => {
+    //     console.log(err.message);
+    // });
+
+    return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async () => {
+    if (typeof window === 'undefined') {
+        //server
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: {
+                    Host: 'ticketing.dev',
+                },
+            }
+        ).catch((err) => {
+            console.log(err.message);
+        });
+        return data;
+    } else {
+        //browser
+        const { data } = axios.get('/api/users/currentuser').catch((err) => {
+            console.log(err.message);
+        });
+        return data;
+    }
+}
+
+export default LandingPage;
