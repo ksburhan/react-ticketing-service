9c440ca81d982864778edcf11158c661492531d3
ksburhan <burhan.koeseler@hotmail.de>
2024-03-08 16:25:43 +0900

    make publisher async

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -8,11 +8,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher conneced to NATS');
 
     const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
+    await publisher.publish({
         id: '123',
         title: 'concert',
         price: 20
