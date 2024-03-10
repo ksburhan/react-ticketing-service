8fcea5134941d1221cb3cba091c50eebe071dd31
ksburhan <burhan.koeseler@hotmail.de>
2024-03-10 14:59:02 +0900

    add variables

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -8,6 +8,8 @@ import {
 } from '@monkeytickets/common';
 
 import { Ticket } from '../models/tickets';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -39,6 +41,13 @@ router.put(
             price: req.body.price
         });
         await ticket.save();
+
+        new TicketUpdatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId
+        });
 
         res.send(ticket);
     });
