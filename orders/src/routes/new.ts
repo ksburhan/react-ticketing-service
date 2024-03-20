535a55b412cd5718006302308d4f61d3901959eb
ksburhan <burhan.koeseler@hotmail.de>
2024-03-20 13:46:11 +0900

    implement create router

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,25 +1,57 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest } from '@monkeytickets/common';
-// import { Order } from '../models/orders';
+import mongoose from 'mongoose';
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@monkeytickets/common';
+
+import { Order } from '../models/orders';
+import { Ticket } from '../models/tickets';
 import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post('/api/orders',
     requireAuth,
     [
-        body('title')
+        body('ticketId')
             .not()
             .isEmpty()
-            .withMessage('Title is required'),
-        body('price')
-            .isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage('TicketId is required'),
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-        const order = {};
-        res.send(order);
+        const { ticketId } = req.body;
+
+        // Find ticket user is trying to order in database
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        // Make sure ticket is not reserved
+        const isReserved = await ticket.isReserved();
+        if (isReserved) {
+            throw new BadRequestError('Ticket is already reserved');
+        }
+
+        // Calculate expiration date for order
+        const expiration = new Date();
+        expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+        // Build order and save to database
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket: ticket
+        });
+        await order.save();
+
+        // Publish event for order creation
+
+        res.status(201).send(order);
     });
 
 export { router as createOrderRouter };
