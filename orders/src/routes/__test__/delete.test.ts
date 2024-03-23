9b1aaebe03bf96fb46e092427caaf84bfbaded93
ksburhan <burhan.koeseler@hotmail.de>
2024-03-23 13:18:57 +0900

    add event tests

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -31,3 +31,27 @@ it('marks an order as cancelled', async () => {
 
     expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
+
+it('publishes an event', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user = global.signin();
+
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
