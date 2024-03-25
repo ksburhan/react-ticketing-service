7619356ed2fcdd9598e440efb5aa58a5f6980f3f
ksburhan <burhan.koeseler@hotmail.de>
2024-03-25 22:10:18 +0900

    add update listener

---
+++ orders/src/events/listeners/ticket-updates-listeners.ts
@@ -0,0 +1,22 @@
+import { Listener, Subjects, TicketUpdatedEvent } from "@monkeytickets/common";
+import { Message } from "node-nats-streaming";
+
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/tickets";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    readonly subject = Subjects.TicketUpdated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+        const ticket = await Ticket.findById(data.id);
+        if (!ticket) {
+            throw new Error('Ticket not found');
+        }
+        const { title, price } = data;
+        ticket.set({ title, price });
+        await ticket.save();
+
+        msg.ack();
+    }
+}
