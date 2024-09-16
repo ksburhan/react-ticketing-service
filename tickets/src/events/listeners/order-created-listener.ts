acc8a9d0f2744dd5c1ec6f8cee6e31952173506f
ksburhan <burhan.koeseler@hotmail.de>
2024-09-16 12:24:02 +0900

    update commonds

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCreatedEvent, Subjects } from "@monkeytickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/tickets";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -21,6 +22,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
         // Save the ticket
         await ticket.save()
+        new TicketUpdatedPublisher(this.client)
 
         // ack the message
         msg.ack()
