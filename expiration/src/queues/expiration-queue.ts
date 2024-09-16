b061df7660b8a53137c7a740241fbc20a51e88f9
ksburhan <burhan.koeseler@hotmail.de>
2024-09-16 15:36:42 +0900

    add listener

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,17 @@
+import Bull from "bull";
+
+interface Payload {
+    orderId: string;
+};
+
+const expirationQueue = new Bull<Payload>('order:expiration', {
+    redis: {
+        host: process.env.REDIS_HOST
+    }
+});
+
+expirationQueue.process(async (job) => {
+    console.log('I want to publish an expiration:complete event for orderId', job.data.orderId)
+});
+
+export { expirationQueue };
