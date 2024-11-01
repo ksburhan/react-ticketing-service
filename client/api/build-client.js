c23eb6f317a27374008deebf4fbbd812f4d70579
ksburhan <burhan.koeseler@hotmail.de>
2024-11-01 21:17:29 +0900

    domain

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ const buildClient = ({ req }) => {
     if (typeof window === 'undefined') {
         //server
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.monkey-ticket.site',
             headers: req.headers
         })
     } else {
